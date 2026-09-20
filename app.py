c0a275f622339fea09ef08f6971e13c09a6562c2
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-20 16:21:48 +0530

    Fix string quotes and update prediction input

--- app.py
+++ app.py
@@ -3,38 +3,31 @@ import joblib
 import pandas as pd
 import os
 
-model = joblib.load("used_car_random_forest.pkl")
-
+model = joblib.load('used_car_random_forest.pkl')
 
 def predict_result(age, kilo):
-
+    
     input_data = pd.DataFrame({
-        "Car_Age": [age],
-        "Kilometers_Driven": [kilo],
+        'Car_Age': [age],
+        'Kilometers_Driven': [kilo],
     })
-
-    prediction = model.predict(input_poly)[0]
-
+    
+    prediction = model.predict(input_data)[0]
     return f"Predicted Car Price Category: {prediction}"
-
 
 demo = gr.Interface(
     fn=predict_result,
-
     inputs=[
         gr.Number(label="Enter the Car Age", minimum=1, value=1),
         gr.Number(label="Enter the Kilometers Driven", minimum=1, value=1),
     ],
-
     outputs=gr.Textbox(label="Prediction"),
-
     title="Used Car Price Category Prediction",
     description="Predict car price category based on Car age and kilometers driven."
 )
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     demo.launch(
-        server_name="0.0.0.0",
+        server_name="0.0.0.0", 
         server_port=int(os.environ.get("PORT", 7860))
     )
