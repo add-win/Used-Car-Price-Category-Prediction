c23864dcc82b5e27de748b9e171b24f8de1d6563
Addwin Alanolikkal <147745866+add-win@users.noreply.github.com>
2026-09-20 16:18:42 +0530

    Change model to predict used car prices
    Updated the prediction model to a used car price model and modified input parameters accordingly.

---
+++ app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import joblib
+import pandas as pd
+import os
+
+model = joblib.load("used_car_random_forest.pkl")
+
+
+def predict_result(age, kilo):
+
+    input_data = pd.DataFrame({
+        "Car_Age": [age],
+        "Kilometers_Driven": [kilo],
+    })
+
+    prediction = model.predict(input_poly)[0]
+
+    return f"Predicted Car Price: ₹{prediction:.2f}"
+
+
+demo = gr.Interface(
+    fn=predict_result,
+
+    inputs=[
+        gr.Number(label="Enter the Car Age", minimum=1, value=1),
+        gr.Number(label="Enter the Kilometers Driven", minimum=1, value=1),
+    ],
+
+    outputs=gr.Textbox(label="Prediction"),
+
+    title="Used Car Price Prediction",
+    description="Predict car price based on Car age and kilometers driven."
+)
+
+
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
